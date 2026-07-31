9d02d1ee28e20f155dd3201338b85476f3a075ec
Álvaro Bátrez <alvarobatrez27@gmail.com>
2026-07-31 11:01:15 -0600

    A2C terminado

--- a2c.m
+++ a2c.m
@@ -16,7 +16,7 @@ p = gcp('nocreate');
 if isempty(p)
     parpool('local', num_envs);
 else
-    fprintf('Parallel environment with %d workers\n', p.NumWorkers);
+    fprintf('Ambientes paralelos con %d workers\n', p.NumWorkers);
 end
 num_inputs = 2;
 actor_layers = {{128, 'relu'} {128, 'relu'} {num_actions, 'softmax'}};
@@ -74,11 +74,40 @@ for episode = 1 : num_episodes
         values = critic.predict(states_norm);
         targets = rewards + (1 - dones) .* gamma .* critic.predict(next_states_norm);
         critic_grads{env} = critic_gradient(critic, states_norm, targets);
+        critic_losses(env) = mean((targets - values).^2);
+        [actor_grads{env}, actor_losses(env), H(env)] = actor_gradient(actor, states_norm, actions_taken, targets, values, beta);
+        returns(env) = sum(rewards);
+        steps(env) = t;
     end
+    avg_actor_grad = average_grad(actor, actor_grads, num_envs);
+    avg_critic_grad = average_grad(critic, critic_grads, num_envs);
+    actor.update_weights(avg_actor_grad);
+    critic.update_weights(avg_critic_grad);
+    mean_steps = mean(steps);
+    total_steps(episode) = mean_steps;
+    total_returns(episode) = mean(returns);
+    total_actor_loss(episode) = mean(actor_losses);
+    total_critic_loss(episode) = mean(critic_losses);
+    total_h(episode) = mean(H);
+    fprintf('Episodio: %d, Pasos: %.1f, Retorno: %.1f, Pérdida Actor: %.4f, Pérdida Crítico: %.4f, Entropía: %.4f\n', episode, mean_steps, total_returns(episode), total_actor_loss(episode), total_critic_loss(episode), total_h(episode))
 end
-function grad = critic_gradient(model, state, target)
+delete(gcp('nocreate'));
+optimal_path = create_path(policy, M);
+subplot(3,2,1), semilogy(1:num_episodes, total_steps), grid on
+title('Pasos'), xlabel('Épocas'), ylabel('Num Pasos')
+subplot(3,2,2), plot(1:num_episodes, total_returns), grid on
+title('Retornos'), xlabel('Épocas'), ylabel('Retorno')
+subplot(3,2,3), plot(1:num_episodes, total_actor_loss), grid on
+title('Pérdida Actor'), xlabel('Épocas'), ylabel('Error')
+subplot(3,2,4), plot(1:num_episodes, total_critic_loss), grid on
+title('Pérdida Crítico'), xlabel('Épocas'), ylabel('Error')
+subplot(3,2,5), plot(1:num_episodes, total_h), grid on
+title('Entropía'), xlabel('Épocas'), ylabel('Entropía')
+draw_maze(M, start_position, optimal_path, [goal_row goal_col])
+function grad = critic_gradient(model, states, target)
+    T = size(states, 1);
     delta = cell(1, model.num_layers);
-    outputs = model.forward(state);
+    outputs = model.forward(states);
     y_pred = outputs{end};
     delta{end} = y_pred - target;
     for i = model.num_layers -1 : -1 : 1
@@ -89,9 +118,47 @@ function grad = critic_gradient(model, state, target)
         delta{i} = derivative .* (delta{i+1} * w);
     end
     grad = cell(1, model.num_layers);
-    E = size(state, 1);
     for i = 1 : model.num_layers
-        grad{i} = (1 / E) * delta{i}' * [ones(E, 1), outputs{i}];
+        grad{i} = (1 / T) * delta{i}' * [ones(T, 1), outputs{i}];
     end
 end
-    
+function [grad, loss, H] = actor_gradient(model, states, actions_taken, targets, values, beta)
+    T = size(states, 1);
+    outputs = model.forward(states);
+    probabilities = outputs{end};
+    log_probabilities = log(probabilities + 1e-6);
+    H = -sum(probabilities .* log_probabilities, 2);
+    indices = sub2ind(size(probabilities), (1:T)', actions_taken);
+    action_log_probabilities = log_probabilities(indices);
+    adv = targets - values;
+    adv = adv / (std(adv) + 1e-8);
+    loss = sum(-adv .* action_log_probabilities - beta * H);
+    one_hot = zeros(size(probabilities));
+    one_hot(indices) = 1;
+    delta = cell(1, model.num_layers);
+    delta{end} = -adv .* (one_hot - probabilities) + beta * probabilities .* (log_probabilities + H);
+    for i = model.num_layers - 1 : -1 : 1
+        layer_activation = model.layers{i}.activation;
+        a = outputs{i+1};
+        derivative = model.activation_derivative(layer_activation, a);
+        w = model.layers{i + 1}.weights(:, 2:end);
+        delta{i} = derivative .* (delta{i + 1} * w);
+    end
+    grad = cell(1, model.num_layers);
+    for i = 1 : model.num_layers
+        grad{i} = delta{i}' * [ones(T, 1), outputs{i}] / T;
+    end
+    loss = loss / T;
+    H = mean(H);
+end
+function grad = average_grad(model, grads, num_envs)
+    grad = grads{1};
+    for env = 2 : num_envs
+        for i = 1 : model.num_layers
+            grad{i} = grad{i} + grads{env}{i};
+        end
+    end
+    for i = 1 : model.num_layers
+        grad{i} = grad{i} / num_envs;
+    end
+end   
